f46a7a8566569079af3912f50782f579df97ef89
abhay <w0848266@myscc.ca>
2024-09-10 12:06:08 -0400

    Created starter Angular file and completed Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'first-assignment';
+  name :string= 'first-assignment';
+  address:string = 'first-assignment';
 }
