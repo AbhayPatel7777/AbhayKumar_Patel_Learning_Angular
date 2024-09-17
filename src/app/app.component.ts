2ebd51df4af649b7add19b06b6a667199f94952a
abhay <w0848266@myscc.ca>
2024-09-17 10:39:36 -0400

    Changed the version to 0.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'first-assignment';
-  name :string= 'first-assignment';
+  name :string= 'first';
   address:string = 'first-assignment';
 }
