df5c5e3f723dd714ec66e377ac73977187801d2d
abhay <w0848266@myscc.ca>
2024-09-27 12:55:32 -0400

    completed steps till the 10

--- src/app/user-list-item/user-list-item.component.ts
+++ src/app/user-list-item/user-list-item.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {User} from "../../share/models/User";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-user-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './user-list-item.component.html',
   styleUrl: './user-list-item.component.css'
 })
