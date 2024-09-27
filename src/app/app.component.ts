d0124b462e3cfcdb8dd8325e7044abd390a6410b
abhay <w0848266@myscc.ca>
2024-09-27 12:32:22 -0400

    completed all the steps till the 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {User} from "../share/models/User";
 import {NgForOf, NgIf} from "@angular/common";
+import {UserListItemComponent} from "./user-list-item/user-list-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgIf],
+  imports: [RouterOutlet, NgForOf, NgIf, UserListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {User} from "../../share/models/User";
+import {UserListItemComponent} from "../user-list-item/user-list-item.component";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [
+    UserListItemComponent,
+    NgClass
+  ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent {
+  userList: User[]=[
+    {id:1,firstName:"Abhay",lastName:"Patel",department:"Computer Programming",isAdmin:false,PhoneNumber:87438573294232},
+    {id:2,firstName:"Rushabh",lastName:"Parekh",department:"Computer Programming",isAdmin:true,PhoneNumber:383247264632},
+    {id:3,firstName:"Rohan",lastName:"Parmar",department:"Computer Programming",isAdmin:false,PhoneNumber:232333221221},
+    {id:4,firstName:"Akshat",lastName:"Rathod",department:"Networking",isAdmin:false,PhoneNumber:98738623992}
 
+  ]
 }
