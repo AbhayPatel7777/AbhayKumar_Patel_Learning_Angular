1666cd4373eeb01208c57ef4c2d7882a21775355
abhay <w0848266@myscc.ca>
2024-10-04 13:22:45 -0400

    completed the steps till the 7

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import {Observable, of} from "rxjs";
+import {User} from "../../share/models/User";
+import {userList} from "../../share/mockUser";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private userList:User[] = userList;
 
   constructor() { }
+  getUsers():Observable<User[]>{
+    return of(userList);
+  }
+
+  addUser(newUser:User):Observable<User[]>{
+    this.userList.push(newUser);
+    return of(this.userList);
+  }
+
+  updateUser(updateUser:User):Observable<User[]>{
+    const index = this.userList.findIndex(abh=>abh.firstName === updateUser.firstName);
+    if (index !== -1) {
+      this.userList[index]=updateUser;
+    }
+    return of(this.userList);
+  }
+  deleteUser(deletefirstName: string):Observable<User[]>{
+    this.userList = this.userList.filter(abh=>abh.firstName !== deletefirstName);
+    return of(this.userList);
+  }
+  getUser(readfirstName:string):Observable<User | undefined>{
+    const hero =this.userList.find(abh=>abh.firstName === readfirstName);
+    return of(hero);
+  }
+
 }
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from "../../share/models/User";
 import {UserListItemComponent} from "../user-list-item/user-list-item.component";
 import {NgClass, NgForOf} from "@angular/common";
+import {UserService} from "../services/user.service";
 
 @Component({
   selector: 'app-user-list',
@@ -14,6 +15,22 @@ import {NgClass, NgForOf} from "@angular/common";
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit{
+  displayColumns:string[] = ['id', 'firstName', 'lastName', 'department', 'isAdmin?', 'PhoneNumber']
+  MarvelComics:User[]=[];
 
+
+  constructor(private userService:UserService) {
+  }
+  ngOnInit() {
+    this.userService.getUsers().subscribe({
+      next:(data:User[])=> this.MarvelComics = data,
+      error: err => console.log("Fetching Error",err),
+      complete:() => console.log("data fetched"),
+    })
+  }
+  selectedUser?:User;
+  selectUser(user:User):void {
+    this.selectedUser = user;
+  }
 }
