6eb7d2c4a11afcae195cc43bcd6fc7cb9571119a
abhay <w0848266@myscc.ca>
2024-10-04 12:47:23 -0400

    created the two new folder one is services and the another is mock

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -15,11 +15,5 @@ import {NgClass, NgForOf} from "@angular/common";
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent {
-  userList: User[]=[
-    {id:1,firstName:"Abhay",lastName:"Patel",department:"Computer Programming",isAdmin:false,PhoneNumber:87438573294232},
-    {id:2,firstName:"Rushabh",lastName:"Parekh",department:"Computer Programming",isAdmin:true,PhoneNumber:383247264632},
-    {id:3,firstName:"Rohan",lastName:"Parmar",department:"Computer Programming",isAdmin:false,PhoneNumber:232333221221},
-    {id:4,firstName:"Akshat",lastName:"Rathod",department:"Networking",isAdmin:false,PhoneNumber:98738623992}
 
-  ]
 }
---
+++ src/share/mockUser.ts
@@ -0,0 +1,9 @@
+import {User} from "./models/User";
+
+export const userList: User[]=[
+  {id:1,firstName:"Abhay",lastName:"Patel",department:"Computer Programming",isAdmin:false,PhoneNumber:87438573294232},
+  {id:2,firstName:"Rushabh",lastName:"Parekh",department:"Computer Programming",isAdmin:true,PhoneNumber:383247264632},
+  {id:3,firstName:"Rohan",lastName:"Parmar",department:"Computer Programming",isAdmin:false,PhoneNumber:232333221221},
+  {id:4,firstName:"Akshat",lastName:"Rathod",department:"Networking",isAdmin:false,PhoneNumber:98738623992}
+
+]
