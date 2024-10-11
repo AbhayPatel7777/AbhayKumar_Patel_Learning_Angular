941f1b40c5ffa5f5711cee35ada8a0faa77e73a7
abhay <w0848266@myscc.ca>
2024-10-11 17:10:48 -0400

    Completed the step 4 and the step 5 as per the instruction

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {UserListItemComponent} from "./app/user-list-item/user-list-item.component";
+import {UserListComponent} from "./app/user-list/user-list.component";
+import * as path from "node:path";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes =[
+{path:'', redirectTo: '/userList', pathMatch: 'full'}, //default route
+{ path: 'userList', component: UserListComponent },
+{ path: 'userList/:id', component: UserListItemComponent },
+];
+bootstrapApplication(AppComponent, {providers:[provideRouter(routes)]})
+  .then(r=>console.log("sucessful"));
