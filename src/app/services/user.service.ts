9ee79cbcdc7e9081ff4525ec5e817e4e490aa102
abhay <w0848266@myscc.ca>
2024-11-08 14:53:08 -0500

    completed the assignment till the 7 step

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,38 +1,43 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from "rxjs";
+import {catchError, Observable, of, throwError} from "rxjs";
 import {User} from "../../share/models/User";
 import {userList} from "../../share/mockUser";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private apiUrl = 'api/user'
   private userList:User[] = userList;
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
   getUsers():Observable<User[]>{
-    return of(this.userList);
+    return this.http.get<User[]>(this.apiUrl).pipe(catchError(this.handelError));
   }
 
   addUser(newUser:User):Observable<User>{
-    this.userList.push(newUser);
-    return of(newUser);
+    newUser.id = <number>this.generateNewId();
+    return this.http.post<User>(this.apiUrl,newUser).pipe(catchError(this.handelError));
   }
 
   updateUser(updateUser:User):Observable<User | undefined>{
-    const index = this.userList.findIndex(User=>User.firstName === updateUser.firstName);
-    if (index > -1) {
-      this.userList[index]=updateUser;
-      return of(updateUser);
-    }
-    return of(undefined);
+    const url = `${this.apiUrl}/${updateUser.id}`;
+    return this.http.post<User>(url,updateUser).pipe(catchError(this.handelError));
   }
 
-  getUserbyId(id: number):Observable<User | undefined>{
-    return of(this.userList.find(User => User.id === id));
+  getUserbyId(id: number):Observable<any>{
+    return this.http.get<User>(`${this.apiUrl}/${userList}`).pipe(catchError(this.handelError));
   }
 
   generateNewId() {
-    return this.userList.length >0 ? Math.max(...this.userList.map(userList => userList.id)) +1 : 1;
+    return this.userList.length > 0 ? Math.max(...this.userList.map(userList => userList.id)) + 1 : 1;
+  }
+  private handelError(error: HttpErrorResponse){
+    console.error('API error:' ,error);
+    return throwError(() => new Error('Server error ,please try again'));
+
   }
 }
