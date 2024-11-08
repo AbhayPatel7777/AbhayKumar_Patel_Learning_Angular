5098551be376d34e251e779e3cb67a1a5af01cb4
abhay <w0848266@myscc.ca>
2024-11-08 14:28:34 -0500

    completed the steps till the 6

---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,50 @@
+import {User} from "../../share/models/User";
+import {InMemoryDbService} from "angular-in-memory-web-api";
+import {Observable} from "rxjs";
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb():{userList:User[]} {
+
+    const userList: User[] = [
+        {
+          id: 1,
+          firstName: "Abhay",
+          lastName: "Patel",
+          department: "Computer Programming",
+          isAdmin: false,
+          PhoneNumber: 87438573294232,
+          images: "/assets/mercedesCclass.webp"
+        },
+        {
+          id: 2,
+          firstName: "Rushabh",
+          lastName: "Parekh",
+          department: "Computer Programming",
+          isAdmin: true,
+          PhoneNumber: 383247264632,
+          images: "/assets/MustangGt.webp"
+        },
+        {
+          id: 3,
+          firstName: "Rohan",
+          lastName: "Parmar",
+          department: "Computer Programming",
+          isAdmin: false,
+          PhoneNumber: 232333221221,
+          images: "/assets/Volvoxc90.avif"
+        },
+        {
+          id: 4,
+          firstName: "Akshat",
+          lastName: "Rathod",
+          department: "Networking",
+          isAdmin: false,
+          PhoneNumber: 98738623992,
+          images: "/assets/Lexus.webp"
+        }
+
+      ];
+    return {userList};
+
+  }
+}
